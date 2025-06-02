e11f84e0e72851d88af5da9487d73c05977239bd
heerokim03 <radiodj03@gmail.com>
2025-06-03 02:18:45 +0900

    최종 버전 완료

--- src/TopWinners.jsx
+++ src/TopWinners.jsx
@@ -5,15 +5,17 @@ function TopWinners({ onBack }) {
   const [topWinners, setTopWinners] = useState([]);
 
   useEffect(() => {
-    fetch('https://worldcup-tracker-default-rtdb.firebaseio.com/topWinners.json')
+    fetch('https://worldcup-tracker-default-rtdb.firebaseio.com/winners.json')
       .then((res) => res.json())
       .then((data) => {
         const sorted = Object.entries(data || {})
-          .sort(([, a], [, b]) => b.winCount - a.winCount)
+          .sort(([, a], [, b]) => b - a)
           .slice(0, 3)
-          .map(([id, info], index) => ({
+          .map(([id, count], index) => ({
+            id,
             rank: `${index + 1}위`,
-            ...info,
+            image: `/images/candidate${id}.jpg`,
+            winCount: count,
           }));
         setTopWinners(sorted);
       });
@@ -24,7 +26,7 @@ function TopWinners({ onBack }) {
       {topWinners.map((item) => (
         <div key={item.id} className="top-winner-item">
           <h3>{item.rank}</h3>
-          <img src={item.image} alt={item.name} />
+          <img src={item.image} alt={`후보 ${item.id}`} />
           <p>{item.winCount}회 우승</p>
         </div>
       ))}
--- src/Winner.jsx
+++ src/Winner.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
 
-function Winner({ winner, onRestart, onShowTop }) {
+function Winner({ winner, onRestart, onShowTopWinners }) {
   return (
     <div className="winner-screen animate-pop">
       <h2 className="winner-title">🏆 최종 우승자 🏆</h2>
-      <img src={winner.image} alt={winner.name} className="winner-image" />
+      <img src={winner.image} alt={`후보 ${winner.id}`} className="winner-image" />
       <button className="restart-button" onClick={onRestart}>다시 시작하기</button>
-      <button className="top-winner-button" onClick={onShowTop}>최다 우승자 TOP 3 보기</button>
+      <button className="top-winner-button" onClick={onShowTopWinners}>최다 우승자 TOP 3 보기</button>
     </div>
   );
 }
